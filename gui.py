82e2b4de7c1a89ccd4cd8bc89b06b53291c868f3
jserpe22 <91560580+jserpe22@users.noreply.github.com>
2021-10-25 22:49:01 -0400

    Update gui.py
    Created button grid, clicking on one will just change it from empty to w or b.

--- gui.py
+++ gui.py
@@ -1,32 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
 
-root = TK()
-root.title('REVERSI')
 
 turn = 0
-
 def buttonClick(b):
+    global turn
     """
     Triggered on button click - makeMove(self, player: str, posn: Posn):
     Should first check if move is valid - moveLegal(self, player: str, posn: Posn) -> bool:
     Right now will just change the text
     """
-    if turn%2 = 0:
-        b.text = "w"
+    if turn%2 == 0:
+        b["text"] = "w"
     else:
-        b.text = "b"
+        b["text"] = "b"
+    turn += 1
+
+def main():
+    root = Tk()
+    root.title('REVERSI')
+
+    #buttons - will pass in board size 
+    rows = 8
+    cols = 8
+    buttons = [[0 for x in range(rows)] for x in range(cols)]
+    for row in range(rows):
+        for col in range (cols):
+            buttons[row][col] = Button(root, text=" ", font=("Helvetica", 20), height=2, width=4, command=lambda row=row, col=col: buttonClick(buttons[row][col]))
+            buttons[row][col].grid(row = row, column = col)
 
 
+    root.mainloop()
 
-#buttons - will pass in board size 
-buttons=dict()
-rows = 8
-cols = 8
-for row in range(rows):
-    for col in range (cols):
-        buttons[row][col] = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, command=lambda: buttonClick(buttons[row][col]))
-        buttons[row][col].grid(row = row, column = col)
-
-
-root.mainloop()
+main()
