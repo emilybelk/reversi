c38cbb736af5240e4d5028d21190b898e0328eea
elaineslau <elainelaux323@gmail.com>
2021-11-13 01:02:04 -0500

    implemented database and user registration info

--- gui.py
+++ gui.py
@@ -1,3 +1,8 @@
+import mysql.connector
+import argparse
+import os
+from datetime import datetime
+
 from tkinter import *
 from tkinter import messagebox
 from game import Game
@@ -5,6 +10,25 @@ from board import Board, Posn, Status
 
 
 turn = 0
+
+#********* database ***********
+# Loads data into the database
+# returns connection to the database
+def init():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root"
+    )  
+    mydb.autocommit = True
+    mycursor = mydb.cursor()
+
+    load_ddl_str = open("ddl.sql").read()
+    load_ddl_data = load_ddl_str.split(';')
+
+    for i in load_ddl_data:
+        mycursor.execute(i)
+
+    return mydb
 
 class mainMenu:
     """
@@ -31,6 +55,23 @@ class mainMenu:
         print("username entered :", username.get())
         print("password entered :", password.get())
         self.root.destroy()
+
+
+    def registerUser(mydb, username, password):
+    # credit to https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-execute.html
+        mycursor = mydb.cursor()
+        mycursor.execute("USE reversi")
+        insert_stmt = ("INSERT INTO user (username, password) VALUES (%s, %s)")
+        data = (username, password)
+        try: 
+            mycursor.execute(insert_stmt, data)
+            print("Updated table")
+            print("{:<15}{:<15}".format("username", "password"))
+            mycursor.execute("SELECT * FROM user")
+            for x in mycursor:
+                print("{:<15}{:<15}".format(x[0],x[1]))
+        except mysql.connector.Error as err:
+            print("Error: {}".format(err))
 
 
     def main(self):
